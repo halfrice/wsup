58d472732fd8cd597481371f57762ba85fd0990e
halfrice <neel@surrus.net>
2017-07-14 19:19:23 -0700

    phone and tablet friendly slider

--- src/app.js
+++ src/app.js
@@ -26,92 +26,138 @@ const css = require('./app.scss');
     // $('body').scrollspy({target: '.navbar'});
 
     // SLIDER
-    var numOfSlides = $('.image').length;
+    var $slider = $('.x-slider');
+    var $slideBGs = $('.slide__bg');
+    var diff;
     var currentSlide = 0;
-    var autoSlider;
+    var numOfSlides = $('.slide').length-1;
+    var animating = false;
+    var animationTime = 500;
+    var autoSlideTimeout;
+    var autoSlideDelay = 6000;
+    var $pagination = $(".slider-pagi");
 
     // build pagination bullets
-    $.each($('.slider ul li'), function() {
-      var li = document.createElement('li');
-      $('.slider-pagination ul').append(li);
-    });
-    // set initial slider state
-    currentSlide = 1;
-    bulletUpdate();
-    counterUpdate();
+    function createBullets() {
+      for (var i = 0; i < numOfSlides+1; i++) {
+        var $li = $("<li class='slider-pagi__elem'></li>");
+        $li.addClass("slider-pagi__elem-"+i).data("page", i);
+        if (!i) $li.addClass("active");
+        $pagination.append($li);
+      }
+    };
+    createBullets();
 
-    // bundle tick/update functions into setInterval 
-    function sliderPlay() {
-      slideRight();
-      slideUpdate();
-      bulletUpdate();
-      counterUpdate();
-    }
-    // set autoSlider to a var for e.g. clearInterval on hover
-    autoSlider = setInterval(sliderPlay, 6000);
-    $('.slider').hover(function() {
-      $(this).addClass('active');
-      clearInterval(autoSlider);
-    }, function() {
-      $(this).removeClass('active');
-      autoSlider = setInterval(sliderPlay, 6000);
-    })
+    function manageControls() {
+      $('.slider-control').removeClass('inactive');
+      if (!currentSlide) $('.slider-control.left').addClass('inactive');
+      if (currentSlide === numOfSlides) $('.slider-control.right').addClass('inactive');
+    };
 
-    function slideUpdate() {
-      $('.slideshow li').removeClass('active');
-      $('.slideshow li:eq('+(currentSlide-1)+')').addClass('active');
+
+    function slideOnEm() {
+      autoSlideTimeout = setTimeout(function() {
+        currentSlide++
+        if (currentSlide > numOfSlides) currentSlide = 0;
+        changeSlides();
+      }, autoSlideDelay);
     }
-    function bulletUpdate() {
-      $('.slider-pagination ul li').removeClass('active');
-      $('.slider-pagination ul li:eq('+(currentSlide-1)+')').addClass('active');
+    slideOnEm();
+
+    function changeSlides(instant) {
+      if (!instant) {
+        animating = true;
+        manageControls();
+        $slider.addClass('animating');
+        $slider.css('top');
+        $('.slide').removeClass('active');
+        $('.slide-'+currentSlide).addClass('active');
+        setTimeout(function() {
+          $slider.removeClass('animating');
+          animating = false;
+        }, animationTime);
+      }
+      window.clearTimeout(autoSlideTimeout);
+      $('.slider-pagi__elem').removeClass('active');
+      $('.slider-pagi__elem-'+currentSlide).addClass('active');
+      $slider.css('transform', 'translate3d('+ -currentSlide*100 +'%,0,0)');
+      $slideBGs.css('transform', 'translate3d('+ currentSlide*50 +'%,0,0)');
+      diff = 0;
+      slideOnEm();
+    }
+
+    function slideLeft() {
+      if (animating) return;
+      if (currentSlide > 0) currentSlide--;
     }
     function slideRight() {
-      if (currentSlide >= numOfSlides) currentSlide = 1;
-      else currentSlide++;
-    }
-    function slideLeft() {
-      if (currentSlide <= 1) currentSlide = totalSlides;
-      else currentSlide--;
+      if (animating) return;
+      if (currentSlide < numOfSlides) currentSlide++;
     }
 
-    function counterUpdate(){
-      $('.slider-counter').html(currentSlide + ' / ' + numOfSlides);
-    }
+    $(document).on('mousedown touchstart', '.x-slider', function(e) {
+      console.log('mousedown touchstart');
+      if (animating) return;
+      window.clearTimeout(autoSlideTimeout);
+      var startX = e.pageX || e.originalEvent.touches[0].pageX;
+      var winW = $(window).width();
+      diff = 0;
 
+      $(document).on('mousemove touchmove', function(e) {
+        console.log('mousemove touchmove');
+        var x = e.pageX || e.originalEvent.touches[0].pageX;
+        diff = (startX - x) / winW * 70;
+        if ((!currentSlide && diff < 0) || 
+            (currentSlide === numOfSlides && diff > 0)) diff /= 2;
+        $slider.css("transform", "translate3d("+ (-currentSlide*100 - diff) +"%,0,0)");
+        $slideBGs.css("transform", "translate3d("+ (currentSlide*50 + diff/2) +"%,0,0)");
+      });
+    });
 
-    // SLIDER
-    // $('.slider ul#slider-list').width(sliderWidth*totalSlides);
-    // $('#next').click(function(){
-    //   slideRight();
-    // });
-    // $('#previous').click(function(){
-    //   slideLeft();
-    // });
+    $(document).on("mouseup touchend", function(e) {
+      console.log('mouseup touchend')
+      $(document).off("mousemove touchmove");
+      console.log('mousemove touchmove')
+      if (animating) return;
+      if (!diff) {
+        changeSlides(true);
+        return;
+      }
+      if (diff > -8 && diff < 8) {
+        changeSlides();
+        return;
+      }
+      if (diff <= -8) {
+        slideLeft();
+      }
+      if (diff >= 8) {
+        slideRight();
+      }
+    });
 
-    // $.each($('.slider ul li'), function() {
-    //   var color = $(this).attr('data-color');
-    //   $(this).css('background-color', color);
-    //   var li = document.createElement('li');
-    //   $('#pagination ul').append(li);
-    // });
+    $(document).on('click', '.slider-control', function() {
+      console.log('clicked on .slider-control');
+      if ($(this).hasClass('left')) {
+        slideLeft();
+      } else {
+        slideRight();
+      }
+    });
 
-    // // GET MOUSE POS
-    // var picsOffset = $('.slider').offset();
-    // var picsMousePos = { x: -1, y: -1 };
-    // $(document).mousemove(function(event) {
-    //   picsMousePos.x = event.pageX-picsOffset.left;
-    //   picsMousePos.y = event.pageY+picsOffset.top;
-    //   displayPicStats();
-    // });
+    $(document).on('click', '.slider-pagi__elem', function() {
+      console.log('clicked on .slider-pagi__elem');
+      currentSlide = $(this).data('page');
+      changeSlides();
+    });
 
   });
 
   /*******************************************************************
   APP CONFIG
   *******************************************************************/
-  var sliderWidth = $('.slider').width();
-  var sliderHeight = $('.slider').height();
-  var sliderCanvas = {};
+  // var sliderWidth = $('.slider').width();
+  // var sliderHeight = $('.slider').height();
+  // var sliderCanvas = {};
 
   
 }));
